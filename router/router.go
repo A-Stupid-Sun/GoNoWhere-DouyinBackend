a285749ff95456ee0655167f95ce329cd76bcd23
mixinju <mixinju@outlook.com>
2022-05-25 23:46:07 +0800

    加日志

--- router/router.go
+++ router/router.go
@@ -16,11 +16,11 @@ func InitRouter() *gin.Engine {
 
 	r.POST("/douyin/user/register/", v1.UserController.Register) //用户注册
 	r.POST("/douyin/user/login/", v1.UserController.Login)       //用户登录
+	r.GET("/douyin/feed/", v1.FeedController.Feed)               //视频流接口 token 可有可无
 
-	// 需要鉴权token
 	auth := r.Group("", middleware.JWTToken())
 	{
-		r.GET("/douyin/feed/", v1.FeedController.Feed)                                  //视频流接口
+		// 需要鉴权token
 		auth.POST("/douyin/publish/action/", v1.PublishController.Publish)              //用户投稿
 		auth.GET("/douyin/publish/list/", v1.PublishController.PublishList)             //发布列表
 		auth.GET("/douyin/user/", v1.UserController.Info)                               //用户信息
